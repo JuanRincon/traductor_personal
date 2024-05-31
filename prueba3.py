b3081ba4675e5f3787675a30b183899012501572
juan <juan@DESKTOP-OJS0JKN.localdomain>
2024-05-31 12:02:56 -0500

    Vocabulary english to spanish

--- prueba3.py
+++ prueba3.py
@@ -1,29 +1,32 @@
 from random import randint
 from prueba2 import b
 
+tecla = ""
+
 def elige_pares():
-	a = 0
-	a = randint(1, 111)
-	if a%2!=0:
-	    return(a)	
+	b = randint(1, 111)
+	if b%2==0:
+		a = b
+		return(a)
 	else:
-	    elige_pares()	
-	return(a)
+		print("b es igual: ", b)
+		b -= 1
+		a = b
+		print("a es igual: ", a)
+		return(a)
 
 print("Welcome to english vocabulary")
 
-tecla = ""
-
-a = elige_pares()
 
 while (tecla !='x'):
 	print("La palabra a traducir es: ")
-	print("a es igual a: ", a)
+	a = elige_pares()
+	print("En este caso a es igual: ", a)
 	d = str(b[a])
 	#d = d[:-1]
 	print(d, "\n")
 	c = input("Ingrese la traducción: ")
-	if (b[a+1] == (c+',')) | (b[a+2] == (c+',')) | (b[a+3] == (c+',')):
+	if (b[a+1] == c):
 		print("Correcto \n")
 	else:
 		print("Incorrecto.\nLa palabra correcta es: ", b[a+1], "\n")
