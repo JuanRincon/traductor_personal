9efec70d413596c9492eb2754adc9c0d0b5a26a6
juan <juan@DESKTOP-OJS0JKN.localdomain>
2024-06-04 13:33:49 -0500

    Usando un documento para eng-spa como para spa-eng

--- prueba2.py
+++ prueba2.py
@@ -1,18 +1,10 @@
 iden = open("/mnt/d/DiscoExtraible/Estudio/Ingles/Vocabulario.txt")
-iden2 = open("Voc2")
 
 origEng = ""
 for i in iden: 
 	origEng += i
 
-origSpa= ""
-for i in iden2:
-	origSpa += i
-
 iden.close()
-iden2.close()
-
-#print(a)
 
 # a es una variable tipo string
 eng = origEng
@@ -20,5 +12,3 @@ eng = origEng
 # convertimos b de cadena a lista	
 eng = eng.split()
 
-spa= origSpa
-spa= spa.split()
--- prueba3.py
+++ prueba3.py
@@ -1,5 +1,5 @@
 from random import randint
-from prueba2 import eng,spa
+from prueba2 import eng
 from os import system as sys
 
 sys('clear')
@@ -16,58 +16,53 @@ def welcome():
 	print("")
 	print("")
 
-def elige_pares():
-	b = randint(1, 111)
-	if b%2==0:
-		a = b
-		return(a)
-	else:
-		b -= 1
-		a = b
-		return(a)
-	return(cosa)
-
 def select_lang():
 	print("  ##  Press:  ##")
 	print("")
-	print("|- E - for vocabulary english to spanish -|")
-	print("|- S - for vocabulary spanish to english -|\n")
+	print("|- E - for [E]nglish to spanish vocabulary -|")
+	print("|- S - for [S]panish to english vocabulary -|\n")
 	sele = input("")
-	#return(sele)
-	if (sele == 'e'):
-		lang = eng
-	elif (sele == 'E'):
-		lang = eng
-	elif (sele == 's'):
-		lang = spa
-	elif (sele == 'S'):
-		lang = spa
+	elige_pares(sele)
+	return(sele)
+
+def elige_pares(x):
+	b = randint(0, (len(eng)-1))
+	a = 0
+	if ((b%2==0) and (x == 'e' or x == 'E')):
+		a = b + 1
+	elif ((b%2!=0) and (x == 'e' or x == 'E')):
+		b -= 1
+		a = b + 1
+	elif ((b%2==0) and (x == 's' or x == 'S')):
+		b -= 1
+		a = b - 1
+	elif ((b%2!=0) and (x == 's' or x == 'S')):
+		a = b - 1
 	else:
 		select_lang()
-	return(lang)
+	play_eng_esp(a,b)
 
-def play_eng_esp():
-	lang = select_lang()
+def play_eng_esp(a,b):
+	lang = eng
 	tecla = ""
 	while (tecla !='x'):
 		if (tecla == 'z'):
-			lang = select_lang()
-		a = elige_pares()
+			select_lang()
 		print("La palabra a traducir es: ")
-		d = str(lang[a])
+		d = str(lang[b])
 		print(d, "\n")
 		c = input("Ingrese la traducción: ")
-		if (lang[a+1] == c):
+		if (lang[a] == c):
 			print("Correcto \n")
 		else:
-			print("Incorrecto.\nLa palabra correcta es: ", lang[a+1], "\n")
+			print("Incorrecto.\nLa palabra correcta es: ", lang[a], "\n")
 		print("Ingrese \"x\" para salir \"z\" para cambiar el lenguaje : ")
 		print("o presione otra letra para continuar \n")
 		tecla = input("")	
 
 def main():
 	welcome()
-	play_eng_esp()
+	select_lang()
 
 if __name__=='__main__':
 	main()		
