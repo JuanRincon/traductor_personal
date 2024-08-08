eb116bf519dde6c38df5ddb3862dcb38dbf140b1
juan <Curum@DESKTOP-OJS0JKN>
2024-08-08 11:31:42 -0500

    Empieza a funcionar MVC

--- prueba3.py
+++ prueba3.py
@@ -4,66 +4,22 @@ from os import system as sys
 
 sys('clear')
 
-"""
-def select_lang():
-	print("  ##  Press:  ##")
-	print("")
-	print("|- E - for [E]nglish to spanish vocabulary -|")
-	print("|- S - for [S]panish to english vocabulary -|\n")
-	sele = input("")
-	elige_pares(sele)
-"""
-
-def select_lang(o):
-    if o == "Eng":
-        print("English")
-    elif o == "Spa":
-        print("Spanish")
-    else:
-        print("Error")
-    elige_pares(o)
-
 def elige_pares(x):
-	b = randint(0, (len(eng)-1))
-	a = 0
-	if ((b%2==0) and (x == 'e' or x == 'E')):
-		a = b + 1
-	elif ((b%2!=0) and (x == 'e' or x == 'E')):
-		b -= 1
-		a = b + 1
-	elif ((b%2==0) and (x == 's' or x == 'S')):
-		b -= 1
-		a = b - 1
-	elif ((b%2!=0) and (x == 's' or x == 'S')):
-		a = b - 1
-	else:
-		print("error")
-	play_eng_esp(a,b,x)
-
-def play_eng_esp(a,b,y):
-	lang = eng
-	tecla = ""
-	print("La palabra a traducir es: ")
-	d = str(lang[b])
-	print(d, "\n")
-	c = input("Ingrese la traducción: ")
-	if (lang[a] == c):
-		print("Correcto \n")
-	else:
-		print("Incorrecto.\nLa palabra correcta es: ", lang[a], "\n")
-	print("Ingrese \"x\" para salir \"z\" para cambiar el lenguaje : ")
-	print("o presione otra letra para continuar \n")
-	tecla = input("")	
-	opciones(tecla)
-
-def opciones(m):
-	if (m == 'z'):
-		select_lang()
-	#elif(m == 'x'):
-		#despedida()
-	else:
-		elige_pares(y)
-
+    b = randint(0, (len(eng)-1))
+    a = 0
+    if ((b%2==0) and (x == 'Eng')):
+        a = b + 1
+    elif ((b%2!=0) and (x == 'Eng')):
+        b -= 1
+        a = b + 1
+    elif ((b%2==0) and (x == 'Spa')):
+        b -= 1
+        a = b - 1
+    elif ((b%2!=0) and (x == 'Spa')):
+        a = b - 1
+    else:
+        print("error")
+    return(a,b,x)
 
 def main():
 #	welcome()
--- vista.py
+++ vista.py
@@ -1,33 +1,72 @@
 import tkinter
-from prueba3 import *
+import prueba3 
+from random import randint
+from prueba2 import eng
+from os import system as sys
+from prueba3 import elige_pares
+
+sys('clear')
+
+def cambio_texto():
+    Label_texto_intro.configure(text="Seleccione el modo de traducción")
+    boton_cambio_espanol.configure(text="English",command=English_languaje)
+    boton_cambio_espanol.pack(padx=20,pady=5)
+    boton_cambio.configure(text="Spanish",command=Spanish_languaje)
+
+def English_languaje():
+    Label_texto_intro.configure(text="La palabra a traducir es: ")
+    y = "Eng"
+    eleccion = elige_pares(y)
+    eleccion = list(eleccion)
+    c = eleccion[0]
+    b = eleccion[1]
+    play_eng_esp(c,b,y)
+
+def Spanish_languaje():
+    mensaje_label.configure(text="Has elegido Spanish")
+    y = "Spa"
+    eleccion = elige_pares(y)
+    eleccion = list(eleccion)
+    c = eleccion[0]
+    b = eleccion[1]
+    play_eng_esp(c,b,y)
+
+
+def play_eng_esp(c,b,y):
+    lang = eng
+    tecla = ""
+    #print("La palabra a traducir es: ")
+    d = str(lang[b])
+    print(d, "\n")
+    e = input("Ingrese la traducción: ")
+    if (lang[c] == e):
+        print("Correcto \n")
+    else:
+        print("Incorrecto.\nLa palabra correcta es: ", lang[c], "\n")
+    print("Ingrese \"x\" para salir \"z\" para cambiar el lenguaje : ")
+    print("o presione otra letra para continuar \n")
+    tecla = input("")	
+    opciones(tecla)
+
+def opciones(m):
+	if (m == 'z'):
+		select_lang()
+	#elif(m == 'x'):
+		#despedida()
+	else:
+		cambio_texto()
 
 ventana = tkinter.Tk()
 
 nombre_var = tkinter.StringVar()
 
-def English_languaje():
-    mensaje_label.configure(text="Has elegido English")
-    a = "Eng"
-    select_lang(a)
-
-def Spanish_languaje():
-    mensaje_label.configure(text="Has elegido Spanish")
-    a = "Spa"
-    select_lang(a)
-
-def cambio_texto():
-    texto_intro.configure(text="Seleccione el modo de traducción")
-    boton_cambio_espanol.configure(text="English",command=English_languaje)
-    boton_cambio.configure(text="Spanish",command=Spanish_languaje)
-
-texto_intro = tkinter.Label(ventana, text="Welcome to Englis Vocabulary \nYour app to practice your personal vocabulary",font=("Arial",18),padx=20,pady=10)
-texto_intro.pack()
+Label_texto_intro = tkinter.Label(ventana, text="Welcome to Englis Vocabulary \nYour app to practice your personal vocabulary",font=("Arial",18),padx=20,pady=10)
+Label_texto_intro.pack()
 
 boton_cambio = tkinter.Button(ventana, text="Precione para iniciar",font=("Arial",16),command=cambio_texto)
-boton_cambio.pack(padx=20,pady=10)
+boton_cambio.pack(padx=20,pady=5)
 
 boton_cambio_espanol= tkinter.Button(ventana, text="",font=("Arial",16))
-boton_cambio_espanol.pack(padx=20,pady=10)
 
 mensaje_label = tkinter.Label(ventana,text="",font=("Arial",18))
 mensaje_label.pack(padx=20,pady=5)
